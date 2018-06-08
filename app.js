4297e65ca0111724d0bc78d633f2657f9007116f
B.J. Hayes <b.justinhayes@gmail.com>
2018-06-08 14:47:40 -0700

    finished problem 3, passed. Tricky, tricky with those + ' . ' strings

--- app.js
+++ app.js
@@ -33,14 +33,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-  var theProduct = a * b;
-  var theMessage = 'The product of ' + a + ' and ' + b + ' is ' + theProduct + '.';
-  return [theProduct, theMessage];
-}
+// function multiply(a, b) { //eslint-disable-line
+//   var theProduct = a * b;
+//   var theMessage = 'The product of ' + a + ' and ' + b + ' is ' + theProduct + '.';
+//   return [theProduct, theMessage];
+// }
 
 // Here is the test for multiply(); uncomment it to run it
-testMultiply(5,9);
+// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -57,11 +57,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var theSum = a + b + c;
+  var theProduct = a * b * c;
+  var theMessage1 = a + ' and ' + b + ' and ' + c + ' sum to ' + theSum + '.';
+  var theMessage2 = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theProduct + '.';
+  console.log(theMessage1);
+  console.log(theMessage2);
+  return [theSum, theProduct, theMessage1, theMessage2]
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
