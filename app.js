d8f141275bd620b727e06bc2b7740383529dade4
B.J. Hayes <b.justinhayes@gmail.com>
2018-06-08 18:04:29 -0700

    that was tough, but I'm getting to know array positions. Passed

--- app.js
+++ app.js
@@ -83,15 +83,21 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-// var testArray = [2, 3, 4]; //eslint-disable-line
 
-// function sumArray(sumArr) { //eslint-disable-line
+var testArray = [2, 3, 4]; //eslint-disable-line
 
-// }
+function sumArray(sumArr) { //eslint-disable-line
+  var firstElementtotal = sum(testArray[0],testArray[1]);
+  var firstElementtotal2 = sum(firstElementtotal[0],testArray[2]);
+  var secondndElementString = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + firstElementtotal2[0] + ' is their sum.'
+  console.log(secondndElementString);
+
+  return [firstElementtotal2[0], secondndElementString]
+}
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
